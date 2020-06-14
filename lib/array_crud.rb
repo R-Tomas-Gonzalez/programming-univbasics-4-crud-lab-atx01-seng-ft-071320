0742f60a25d16ba815bb2a9ec8b3578e3465ae09
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-14 19:55:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,6 @@ def add_element_to_end_of_array(array, element)
   array.push("arrays!")
 end
 
-add_element_to_end_of_array([], "element")
-
 
 def add_element_to_start_of_array(array, element)
   array2 = ["wow"]
