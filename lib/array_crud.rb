10a8a0d5e0915af72e08f9e7d0793e7f043550f8
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-14 19:54:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,10 +9,10 @@ end
 
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push("element2")
 end
 
-add_element_to_start_of_array([array], "element")
+add_element_to_start_of_array([], "element")
 
 
 def add_element_to_start_of_array(array, element)
